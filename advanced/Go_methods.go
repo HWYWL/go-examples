af1ee5704907fc7d0a49446c25a729227a51dd66
YI <18819478773@163.com>
2018-05-14 17:08:42 +0800

    :bento: 方法使用

---
+++ advanced/Go_methods.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"strconv"
+	"fmt"
+)
+
+// Go 没有类，然而任然可以在结构体类型上定义方法
+type Person struct {
+	name string
+	age int
+}
+
+func (p *Person) sayHello() string {
+	//strconv.Itoa 可以字符串跟int之间的转换类型
+	return "I,m " + p.name + ", this year is " + strconv.Itoa(p.age)
+}
+
+type MyString string
+
+//你可以对包中的任意类型定义任何方法，而不仅仅是针对结构体
+//但是不能对来自其他包的类型或者基础类型定义方法
+func (str MyString) length() int {
+	return len(str)
+}
+
+func main() {
+	p := &Person{"yi", 25}
+	fmt.Println(p.sayHello())
+
+	fmt.Println(MyString("Hello").length())
+}
