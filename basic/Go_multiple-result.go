7cca6cbaf69e89d10282293de415f089c747df29
YI <18819478773@163.com>
2018-05-14 10:18:03 +0800

    :ok_hand: 更新代码，go语言函数返回任意多的返回值

---
+++ basic/Go_multiple-result.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+/*
+	多值返回，函数可以返回任意数量的返回值
+ */
+func main() {
+	var a, b string = "hello", "golang"
+	fmt.Println(swap(a, b))
+}
