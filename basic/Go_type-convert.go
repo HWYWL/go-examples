04252fa464af0ba6bdcadca9da06ea37f25d9399
YI <18819478773@163.com>
2018-05-14 10:32:22 +0800

    :bento: 类型转换

---
+++ basic/Go_type-convert.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+// 类型转换
+func main() {
+	var x, y int = 3, 4
+	// f = 5.291502622129181
+	var f float64 = math.Sqrt(float64(x * y + y * y))
+	var z int = int(f)
+
+	// z = 5
+	fmt.Println(z)
+}
