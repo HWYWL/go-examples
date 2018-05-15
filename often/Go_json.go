6f106681cf48235601ddb94fd6b08f5d08dc0d65
YI <18819478773@163.com>
2018-05-15 14:11:37 +0800

    :ok_hand: 更新代码，json处理

---
+++ often/Go_json.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Student struct {
+	Name string
+	Age int
+	Guake bool
+	Classes []string
+	Price float32
+} 
+
+func main() {
+	st := &Student{
+		"Xiao Ming",
+		16,
+		true,
+		[]string{"语文", "数学", "英语"},
+		9.99,
+	}
+	b, err := json.Marshal(st)
+
+	var jsonstr string
+	if err != nil {
+		fmt.Printf("err was %v", err)
+	}else {
+		//将对象解析为json字符串
+		jsonstr = string(b)
+		fmt.Println(jsonstr)
+	}
+
+	st2 := Student{}
+	//将字符串解析为对象
+	err = json.Unmarshal([]byte(jsonstr), &st2)
+	if err != nil {
+		fmt.Printf("err was %v", err)
+	}else {
+		fmt.Println(st2)
+	}
+}
