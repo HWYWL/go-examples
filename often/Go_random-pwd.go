610ea3ddc70537330e4f0acda83582c7f2fea363
YI <18819478773@163.com>
2018-05-16 09:43:47 +0800

    :ok_hand: 更新代码,随机密码生成

---
+++ often/Go_random-pwd.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"math/rand"
+	"fmt"
+)
+
+// 随机密码生成
+
+//盐
+var letters = []rune("hfislfhasfhfdjskfhisufhifusifusgy")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(b)
+}
+
+func main() {
+	//idusjkahfhshfffhifsfsh
+	fmt.Println(randSeq(22))
+}
