eb4c5c9ee7e1f5c54f54d2603900b53bd1dfec3b
YI <18819478773@163.com>
2018-05-17 15:01:27 +0800

    :ok_hand: 更新代码，计算函数执行时间（使用缓存）

--- often/Go_fibonacci.go
+++ often/Go_fibonacci.go
@@ -5,11 +5,18 @@ import (
 	"time"
 )
 
+const LIM = 51
+
+var fibs [LIM]uint64
+
 func main() {
-	result := 0
+	var result uint64 = 0
 	start := time.Now()
-	for i := 0; i <= 35; i++ {
-		result = fibonacci(i)
+	for i := 0; i < LIM; i++ {
+		//普通版 耗时3m20s
+		//result = fibonacci(i)
+		//缓存版 0s
+		result = fibonaccc(i)
 		fmt.Printf("fibonacci(%d) is: %d\n", i, result)
 	}
 	end := time.Now()
@@ -18,11 +25,27 @@ func main() {
 }
 
 //递归运算
-func fibonacci(n int) (res int) {
+func fibonacci(n int) (res uint64) {
 	if n <= 1 {
 		res = 1
 	} else {
 		res = fibonacci(n-1) + fibonacci(n-2)
 	}
+	return
+}
+
+//缓存版递归运算
+func fibonaccc(n int) (res uint64) {
+	// 检查数组内存中是否存在
+	if fibs[n] != 0 {
+		res = fibs[n]
+		return
+	}
+	if n <= 1 {
+		res = 1
+	} else {
+		res = fibonaccc(n-1) + fibonaccc(n-2)
+	}
+	fibs[n] = res
 	return
 }
